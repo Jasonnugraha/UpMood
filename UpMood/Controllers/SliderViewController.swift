19a7d8e503e8a6a95447ff7b4e57b7c9c58c4870
Pieter Yonathan <pieteryonathan20@gmail.com>
2021-04-06 18:25:02 +0700

    slider wip

--- UpMood/Controllers/SliderViewController.swift
+++ UpMood/Controllers/SliderViewController.swift
@@ -18,8 +18,20 @@ class SliderViewController: UIViewController {
         backslider.backgroundColor = UIColor(red: 0.94, green: 0.95, blue: 0.99, alpha: 1)
         backslider.layer.cornerRadius = 10
         view.addSubview(backslider)
-        backemo.layer.cornerRadius = 35
+      
         
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(x:0, y:0, width: 73, height: 73)
+        gradient.colors = [
+            UIColor(red: 0.43, green: 0.7, blue: 0.93, alpha: 1).cgColor,
+            UIColor(red: 0.25, green: 0.61, blue: 0.91, alpha: 1).cgColor
+        ]
+        gradient.locations = [0, 1]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 0.95)
+        gradient.cornerRadius = 35
+        backemo.layer.addSublayer(gradient)
+        backemo.layer.cornerRadius = 35
         
         emot = ["☺️", "😊", "🙂", "😒", "😓", "😣", "😥", "😨", "😰"]
         labelemo = ["Calm", "Comfortable", "Okay", "Annoyed", "Nervous", "Anxious", "Worried", "Fearful", "Panicked" ]
