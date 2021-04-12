ee0defd54e596bfc2af2abbb0762fc743e6de61d
Jason Nugraha <jason.nugraha97@gmail.com>
2021-04-12 17:14:52 +0700

    fixing the reference

---
+++ UpMood/LogPage/DummyCodeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DummyCodeViewController.swift
+//  UpMood
+//
+//  Created by DHIKA ADITYA ARE on 09/04/21.
+//
+
+import UIKit
+
+class DummyCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func goButtonEx(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goButton", sender: DummyCodeViewController.self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //validasi
+        if segue.identifier == "goButton"{
+            let destinationVc = segue.destination as! LogPageViewController
+            destinationVc.x = 3
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
