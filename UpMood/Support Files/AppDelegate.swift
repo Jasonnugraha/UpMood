496161166adf6244dbe0c54191702b816035ca7b
DhikaAdityaAre <dhikaaditya1@gmail.com>
2021-04-14 10:58:58 +0700

    delete deriveddata

--- UpMood/Support Files/AppDelegate.swift
+++ UpMood/Support Files/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
              fatalError("Unresolved error \(error), \(error.userInfo)")
           }
         })
+        
          
          return container
       }()
